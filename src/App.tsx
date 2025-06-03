a9911cbac6080961cda45f70d54519e222d22d3b
Ferg, Maurício <mauriciogarcia81@hotmail.com>
2025-06-03 14:32:37 -0300

    Posicionando components na App e Global css

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/Countdown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 
 function App() {
   return (
@@ -15,6 +21,34 @@ function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              id='meuInput'
+              type='text'
+              labelText='Qualquer Coisa'
+              placeholder='Digite algo'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <Cycles />
+          </div>
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
+            <DefaultButton icon={<StopCircleIcon />} color='red' />
+          </div>
+        </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
